6111505316c8300d9fd1b92c99bcc2bc0e44d63b
Joby <topjoy89@gmail.com>
2015-10-20 04:50:41 +0530

    Testcode multipaction controller

--- FirstWebSpringApp/src/main/java/com/santosh/spring/ArithmeticController.java
+++ FirstWebSpringApp/src/main/java/com/santosh/spring/ArithmeticController.java
@@ -4,10 +4,18 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.multiaction.MultiActionController;
 
 public class ArithmeticController extends MultiActionController {
+	@RequestMapping(method = RequestMethod.GET)	 
+	public ModelAndView handle(HttpServletRequest req, HttpServletResponse res)
+	{
+		return new ModelAndView("/Home");
+	}
 	
 	public ModelAndView add(HttpServletRequest req,HttpServletResponse res) throws Exception{
 		
